835b84f0f19843c1c2bc4922abef1d8c3eb85f72
Mopsgamer <kisillev020304@gmail.com>
2025-09-10 22:36:50 +0200

    fix: print without loop for non-interactive

--- render.test.ts
+++ render.test.ts
@@ -1,16 +1,9 @@
 import { assertEquals } from "jsr:@std/assert/equals";
 import { Task } from "./main.ts";
-import {
-  list,
-  mutex,
-  newLineCount,
-  render,
-  renderCI,
-  renderer,
-  state,
-} from "./render.ts";
+import { newLineCount, render, renderer, state } from "./render.ts";
 import { bold, magenta, red } from "@std/fmt/colors";
 import { patchOutput } from "./output-patcher.test.ts";
+import { assertArrayIncludes } from "jsr:@std/assert/array-includes";
 
 state.noLoop = true;
 
@@ -22,40 +15,35 @@ Deno.test("render", async () => {
   new Task({ prefix: "TestApp", text: "Operating" }).start();
   new Task({ prefix: "TestApp", text: "Operating" }).start().end("failed");
   render();
-  assertEquals(output, [
+  assertEquals(
+    output.at(-1),
     magenta("- TestApp") + " Operating ...\n" +
-    red("✗ TestApp") + " Operating ... " + bold(red("failed")) + "\n",
-  ]);
-  outputUnpatch();
-});
-
-Deno.test("renderCI", async () => {
-  const { output, outputUnpatch } = patchOutput();
-  list.length = 0;
-  assertEquals(await renderCI(), true);
-  const task = new Task({ prefix: "TestApp", text: "Operating" });
-  assertEquals(await renderCI(), false);
-  task.start();
-  assertEquals(await renderCI(), false);
-  task.end("completed");
-  assertEquals(await renderCI(), true);
+      red("✗ TestApp") + " Operating ... " + bold(red("failed")) + "\n",
+  );
   outputUnpatch();
 });
 
 Deno.test("renderer", async () => {
   const { output, outputUnpatch } = patchOutput();
-  renderer(true);
+  const completed = renderer(true);
+  new Task({ prefix: "TestApp", text: "Operating" });
+  const keeper = new Task({ prefix: "TestApp", text: "Operating" }).start();
+  new Task({ prefix: "TestApp", text: "Operating" }).start().end("failed");
+  await completed;
+  assertArrayIncludes(output, [
+    magenta("- TestApp") + " Operating ...\n",
+    red("✗ TestApp") + " Operating ... " + bold(red("failed")) + "\n",
+  ]);
+  keeper.end("completed");
+  output.length = 0;
+  renderer();
   new Task({ prefix: "TestApp", text: "Operating" });
   new Task({ prefix: "TestApp", text: "Operating" }).start();
   new Task({ prefix: "TestApp", text: "Operating" }).start().end("failed");
-  await mutex.acquire();
-  assertEquals(output, [
-    "\x1b[?25l",
+  assertArrayIncludes(output, [
     magenta("- TestApp") + " Operating ...\n",
     red("✗ TestApp") + " Operating ... " + bold(red("failed")) + "\n",
-    "\x1b[?25h",
   ]);
-  mutex.release();
   outputUnpatch();
 });
 
